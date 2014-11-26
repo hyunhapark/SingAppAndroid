e0f720f7d235438fcf845ba489fe32d27e06293e
HyunHa Park <park@hyunha.org>
2014-11-27 05:51:48 +0900

    added some libs

--- Sing/build.sh
+++ Sing/build.sh
@@ -6,6 +6,28 @@ PKG=com.rameon.sing.opensl
 rm -rf src/com/rameon/sing/opensl
 mkdir -p src/com/rameon/sing/opensl
 
+
+jni/GenerateHeaders.sh \
+	"Math" \
+	"de.jurihock.voicesmith.dsp" \
+	"de/jurihock/voicesmith/dsp" \
+	"Math"
+jni/GenerateHeaders.sh \
+	"KissFFT" \
+	"de.jurihock.voicesmith.dsp" \
+	"de/jurihock/voicesmith/dsp" \
+	"KissFFT"
+jni/GenerateHeaders.sh \
+	"NativeResampleProcessor" \
+	"de.jurihock.voicesmith.dsp.processors" \
+	"de/jurihock/voicesmith/dsp/processors" \
+	"Processors"
+jni/GenerateHeaders.sh \
+	"NativeTimescaleProcessor" \
+	"de.jurihock.voicesmith.dsp.processors" \
+	"de/jurihock/voicesmith/dsp/processors" \
+	"Processors"
+
 swig -java -package $PKG -includeall -verbose -outdir $OUTDIR -c++\
  -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers\
  -I./jni -o jni/java_interface_wrap.cpp opensl_example_interface.i
